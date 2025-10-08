fb43eb78736e09eb31242b51572486a70e4bdede
CRedRiver <mutattom1309@gmail.com>
2025-10-08 08:16:50 +0000

    "update"

--- AimsProject/main_package/Cart.java
+++ AimsProject/main_package/Cart.java
@@ -7,22 +7,23 @@ public class Cart {
     private DigitalVideoDisc itemsOrdered[] = 
         new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
 
-    public void addDigitalVideoDisc(DigitalVideoDisc disc){
-        this.qtyOrdered += 1;
-        if (this.qtyOrdered > MAX_NUMBERS_ORDERED) {
-            System.out.println("The cart is almost full");
-        }
-
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+    if (qtyOrdered >= MAX_NUMBERS_ORDERED) {
+        System.out.println("The cart is almost full");
+        return;
+    }
+        itemsOrdered[qtyOrdered] = disc;
+        qtyOrdered++;
     }
     public void removeDigitalVideoDisc(DigitalVideoDisc disc){
         this.qtyOrdered -= 1;
         System.out.println("The disc has been added");
     }
-    public float totalCost(){
+    public float totalCost() {
         float total_cost = 0;
-        for (int i = 0; i < MAX_NUMBERS_ORDERED;i++){
+        for (int i = 0; i < qtyOrdered; i++) {
             total_cost += itemsOrdered[i].getCost();
         }
         return total_cost;
-    }
+}
 }
